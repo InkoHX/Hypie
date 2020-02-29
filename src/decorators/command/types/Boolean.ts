a86a65b9b049f12446e008b4b5b4d3f5852b277c
InkoHX <inkohx@gmail.com>
2020-02-29 22:55:34 +0900

    Add BooleanResolver

---
+++ src/decorators/command/types/Boolean.ts
@@ -0,0 +1,12 @@
+import { ResolveFunction } from '.'
+
+const toBoolean: ResolveFunction = (data: unknown, paramIndex: number): boolean => {
+  const str = String(data).toLowerCase()
+
+  if (str === 'true') return true
+  if (str === 'false') return false
+
+  throw new Error(`第${paramIndex}引数は"true"または"false"を指定する必要があります。`)
+}
+
+export default toBoolean
--- src/decorators/command/types/index.ts
+++ src/decorators/command/types/index.ts
@@ -1,14 +1,17 @@
+import toBoolean from './Boolean'
 import toNumber from './Number'
 import toString from './String'
 
 export type ResolveFunction = (data: unknown, paramIndex: number) => unknown
 
 export type ResolveType = 'string' |
-'number'
+'number' |
+'boolean'
 
 const resolvers: Record<ResolveType, ResolveFunction> = {
   string: toString,
-  number: toNumber
+  number: toNumber,
+  boolean: toBoolean
 }
 
 export default resolvers
