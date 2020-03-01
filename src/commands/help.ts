005f9e1f446575ace8940ac5a5b980743da13011
InkoHX <inkohx@gmail.com>
2020-03-01 14:59:11 +0900

    Add(command): help

---
+++ src/commands/help.ts
@@ -0,0 +1,13 @@
+/* eslint-disable new-cap */
+import { Command, Client } from '@lib'
+import { Message } from 'discord.js'
+
+export default class HelpCommand extends Command {
+  public constructor (client: Client) {
+    super(client, 'help')
+  }
+
+  public run (message: Message): Promise<Message> {
+    return message.channel.send(this.client.commands.keyArray().join(', '), { code: true })
+  }
+}
