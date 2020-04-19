2dd57f0a3985ab5cc97ce2d2228ca53748ec9adf
InkoHX <inkohx@gmail.com>
2020-04-20 00:52:12 +0900

    fix: ArgumentResolver

--- src/decorators/command/types/TextChannel.ts
+++ src/decorators/command/types/TextChannel.ts
@@ -4,10 +4,7 @@ import { ArgumentResolverFunction } from '.'
 import MentionRegex from '../../../common/MentionRegex'
 
 const toTextChannel: ArgumentResolverFunction = async (data, paramIndex, language, message): Promise<TextChannel> => {
-  const id = MentionRegex.CHANNELS_PATTERN.exec(String(data))?.groups?.id
-
-  if (!id) throw new Error(language.error.resolver.textChannel(paramIndex))
-
+  const id = MentionRegex.CHANNELS_PATTERN.exec(String(data))?.groups?.id ?? String(data)
   const client = message.client
   const textChannel = await client.channels.fetch(id)
     .catch(() => null)
--- src/decorators/command/types/User.ts
+++ src/decorators/command/types/User.ts
@@ -4,10 +4,7 @@ import { ArgumentResolverFunction } from '.'
 import MentionRegex from '../../../common/MentionRegex'
 
 const toUser: ArgumentResolverFunction = async (data, paramIndex, language, message): Promise<User> => {
-  const id = MentionRegex.USERS_PATTERN.exec(String(data))?.groups?.id
-
-  if (!id) throw new Error(language.error.resolver.user(paramIndex))
-
+  const id = MentionRegex.USERS_PATTERN.exec(String(data))?.groups?.id ?? String(data)
   const client = message.client
   const user = await client.users.fetch(id)
     .catch(() => null)
